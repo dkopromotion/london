bdb1eb944bb4ed43e5c9606a60f58ad1be63e85c
dkopromotion <dkopromotion@gmail.com>
2025-04-20 09:41:53 +0200

    y

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.onlyfans-osterreich.com',
+  siteUrl: 'https://www.onlyfans-london.com',
   generateRobotsTxt: true,
   changefreq: 'daily',
   priority: 0.7,
